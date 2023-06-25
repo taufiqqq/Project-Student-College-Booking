8ee1b4117e0f1b1ca6034a8e860bf0f5361cc1c7
Afiq Fahmi <afqifahmi9858@gmail.com>
2023-06-25 20:04:19 +0800

    Update Login

--- login.php
+++ login.php
@@ -62,6 +62,6 @@ if (isset($_SESSION['username'])) {
       </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="assets/css/login.js"></script>
+    <script src="assets/js/login.js"></script>
   </body>
 </html>
